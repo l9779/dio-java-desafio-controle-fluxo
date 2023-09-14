d58700524120654c599f66a156b9377b62773129
Lucas B <lucas.b.dev@proton.me>
2023-09-14 15:11:13 -0300

    commit inicial.

---
+++ src/desafio/Contador.java
@@ -0,0 +1,38 @@
+package desafio;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        try (Scanner terminal = new Scanner(System.in)) {
+            System.out.println("Digite o primeiro parâmetro");
+            int parametroUm = terminal.nextInt();
+            System.out.println("Digite o segundo parâmetro");
+            int parametroDois = terminal.nextInt();
+
+            try {
+                // chamando o método contendo a lógica de contagem
+                contar(parametroUm, parametroDois);
+
+            } catch (ParametrosInvalidosException e) {
+                System.out.println(e + " - O segundo parâmetro deve ser maior que o primeiro");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println(e);
+        }
+
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        // validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if (parametroUm > parametroDois)
+            throw new ParametrosInvalidosException();
+
+        int contagem = parametroDois - parametroUm;
+        // realizar o for para imprimir os números com base na variável contagem
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println(i);
+        }
+    }
+}
